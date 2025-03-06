bc65e43771e2e943ffc04bcedf97f0d524df9279
Sreenand76 <sreenands93@gmail.com>
2025-03-06 11:26:29 +0530

    initialized the structure of the project

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-3xl font-bold flex justify-center'>
+      hello
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,34 @@
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
+    },
+  },
+  plugins: [addVariablesForColors],
+};
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
